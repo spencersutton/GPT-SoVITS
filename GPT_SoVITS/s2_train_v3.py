d27e30ab7f710e0653d5907dea0fe56fcbbbb766
Spencer Sutton <spencer.e.sutton@gmail.com>
2025-11-21 01:33:06 -0500

    rm unused

--- GPT_SoVITS/s2_train_v3.py
+++ GPT_SoVITS/s2_train_v3.py
@@ -46,7 +46,6 @@ torch.backends.cudnn.allow_tf32 = True
 torch.set_float32_matmul_precision(
     "medium"
 )  # 最低精度但最快（也就快一丁点），对于结果造成不了影响
-# from config import pretrained_s2G,pretrained_s2D
 global_step = 0
 
 device = "cpu"  # cuda以外的设备，等mps优化后加入
@@ -103,15 +102,6 @@ def run(rank, n_gpus, hps):
             800,
             900,
             1000,
-            # 1100,
-            # 1200,
-            # 1300,
-            # 1400,
-            # 1500,
-            # 1600,
-            # 1700,
-            # 1800,
-            # 1900,
         ],
         num_replicas=n_gpus,
         rank=rank,
@@ -128,11 +118,6 @@ def run(rank, n_gpus, hps):
         persistent_workers=True,
         prefetch_factor=4,
     )
-    # if rank == 0:
-    #     eval_dataset = TextAudioSpeakerLoader(hps.data.validation_files, hps.data, val=True)
-    #     eval_loader = DataLoader(eval_dataset, num_workers=0, shuffle=False,
-    #                              batch_size=1, pin_memory=True,
-    #                              drop_last=False, collate_fn=collate_fn)
 
     net_g = (
         SynthesizerTrn(
@@ -150,23 +135,13 @@ def run(rank, n_gpus, hps):
         ).to(device)
     )
 
-    # net_d = MultiPeriodDiscriminator(hps.model.use_spectral_norm).cuda(rank) if torch.cuda.is_available() else MultiPeriodDiscriminator(hps.model.use_spectral_norm).to(device)
-    # for name, param in net_g.named_parameters():
-    #     if not param.requires_grad:
-    #         print(name, "not requires_grad")
-
     optim_g = torch.optim.AdamW(
         filter(lambda p: p.requires_grad, net_g.parameters()),  ###默认所有层lr一致
         hps.train.learning_rate,
         betas=hps.train.betas,
         eps=hps.train.eps,
     )
-    # optim_d = torch.optim.AdamW(
-    #     net_d.parameters(),
-    #     hps.train.learning_rate,
-    #     betas=hps.train.betas,
-    #     eps=hps.train.eps,
-    # )
+
     if torch.cuda.is_available():
         net_g = DDP(net_g, device_ids=[rank], find_unused_parameters=True)
         # net_d = DDP(net_d, device_ids=[rank], find_unused_parameters=True)
@@ -175,14 +150,6 @@ def run(rank, n_gpus, hps):
         # net_d = net_d.to(device)
 
     try:  # 如果能加载自动resume
-        # _, _, _, epoch_str = utils.load_checkpoint(
-        #     utils.latest_checkpoint_path("%s/logs_s2_%s" % (hps.data.exp_dir,hps.model.version), "D_*.pth"),
-        #     net_d,
-        #     optim_d,
-        # )  # D多半加载没事
-        # if rank == 0:
-        #     logger.info("loaded D")
-        # _, _, _, epoch_str = utils.load_checkpoint(utils.latest_checkpoint_path(hps.model_dir, "G_*.pth"), net_g, optim_g,load_opt=0)
         _, _, _, epoch_str = utils.load_checkpoint(
             utils.latest_checkpoint_path(
                 f"{hps.data.exp_dir}/logs_s2_{hps.model.version}", "G_*.pth"
@@ -192,10 +159,7 @@ def run(rank, n_gpus, hps):
         )
         epoch_str += 1
         global_step = (epoch_str - 1) * len(train_loader)
-        # epoch_str = 1
-        # global_step = 0
     except:  # 如果首次不能加载，加载pretrain
-        # traceback.print_exc()
         epoch_str = 1
         global_step = 0
         if hps.train.pretrained_s2G not in {"", None} and os.path.exists(
@@ -219,29 +183,12 @@ def run(rank, n_gpus, hps):
                     strict=False,
                 ),
             )  ##测试不加载优化器
-        # if hps.train.pretrained_s2D != ""and hps.train.pretrained_s2D != None and os.path.exists(hps.train.pretrained_s2D):
-        #     if rank == 0:
-        #         logger.info("loaded pretrained %s" % hps.train.pretrained_s2D)
-        #     print(
-        #         net_d.module.load_state_dict(
-        #             torch.load(hps.train.pretrained_s2D, map_location="cpu")["weight"]
-        #         ) if torch.cuda.is_available() else net_d.load_state_dict(
-        #             torch.load(hps.train.pretrained_s2D, map_location="cpu")["weight"]
-        #         )
-        #     )
-
-    # scheduler_g = torch.optim.lr_scheduler.ExponentialLR(optim_g, gamma=hps.train.lr_decay, last_epoch=epoch_str - 2)
-    # scheduler_d = torch.optim.lr_scheduler.ExponentialLR(optim_d, gamma=hps.train.lr_decay, last_epoch=epoch_str - 2)
 
     scheduler_g = torch.optim.lr_scheduler.ExponentialLR(
         optim_g, gamma=hps.train.lr_decay, last_epoch=-1
     )
-    # scheduler_d = torch.optim.lr_scheduler.ExponentialLR(
-    #     optim_d, gamma=hps.train.lr_decay, last_epoch=-1
-    # )
     for _ in range(epoch_str):
         scheduler_g.step()
-        # scheduler_d.step()
 
     scaler = GradScaler(enabled=hps.train.fp16_run)
 
@@ -257,7 +204,6 @@ def run(rank, n_gpus, hps):
                 [optim_g, optim_d],
                 [scheduler_g, scheduler_d],
                 scaler,
-                # [train_loader, eval_loader], logger, [writer, writer_eval])
                 [train_loader, None],
                 logger,
                 [writer, writer_eval],
@@ -303,17 +249,6 @@ def train_and_evaluate(
     global global_step
 
     net_g.train()
-    # net_d.train()
-    # for batch_idx, (
-    #     ssl,
-    #     ssl_lengths,
-    #     spec,
-    #     spec_lengths,
-    #     y,
-    #     y_lengths,
-    #     text,
-    #     text_lengths,
-    # ) in enumerate(tqdm(train_loader)):
     for batch_idx, (
         ssl,
         spec,
@@ -408,14 +343,6 @@ def train_and_evaluate(
                     scalars=scalar_dict,
                 )
 
-            # if global_step % hps.train.eval_interval == 0:
-            #     # evaluate(hps, net_g, eval_loader, writer_eval)
-            #     utils.save_checkpoint(net_g, optim_g, hps.train.learning_rate, epoch,os.path.join(hps.s2_ckpt_dir, "G_{}.pth".format(global_step)),scaler)
-            #     # utils.save_checkpoint(net_d, optim_d, hps.train.learning_rate, epoch,os.path.join(hps.s2_ckpt_dir, "D_{}.pth".format(global_step)),scaler)
-            #     # keep_ckpts = getattr(hps.train, 'keep_ckpts', 3)
-            #     # if keep_ckpts > 0:
-            #     #     utils.clean_checkpoints(path_to_models=hps.s2_ckpt_dir, n_ckpts_to_keep=keep_ckpts, sort_by_time=True)
-
         global_step += 1
     if epoch % hps.train.save_every_epoch == 0 and rank == 0:
         if hps.train.if_save_latest == 0:
@@ -429,15 +356,6 @@ def train_and_evaluate(
                     f"G_{global_step}.pth",
                 ),
             )
-            # utils.save_checkpoint(
-            #     net_d,
-            #     optim_d,
-            #     hps.train.learning_rate,
-            #     epoch,
-            #     os.path.join(
-            #         "%s/logs_s2_%s" % (hps.data.exp_dir,hps.model.version), "D_{}.pth".format(global_step)
-            #     ),
-            # )
         else:
             utils.save_checkpoint(
                 net_g,
@@ -449,15 +367,6 @@ def train_and_evaluate(
                     f"G_{233333333333}.pth",
                 ),
             )
-            # utils.save_checkpoint(
-            #     net_d,
-            #     optim_d,
-            #     hps.train.learning_rate,
-            #     epoch,
-            #     os.path.join(
-            #         "%s/logs_s2_%s" % (hps.data.exp_dir,hps.model.version), "D_{}.pth".format(233333333333)
-            #     ),
-            # )
         if rank == 0 and hps.train.if_save_every_weights:
             if hasattr(net_g, "module"):
                 ckpt = net_g.module.state_dict()
--- GPT_SoVITS/utils.py
+++ GPT_SoVITS/utils.py
@@ -147,39 +147,6 @@ def plot_spectrogram_to_numpy(spectrogram):
     return data
 
 
-def plot_alignment_to_numpy(alignment, info=None):
-    global MATPLOTLIB_FLAG
-    if not MATPLOTLIB_FLAG:
-        import matplotlib as mpl
-
-        mpl.use("Agg")
-        MATPLOTLIB_FLAG = True
-        mpl_logger = logging.getLogger("matplotlib")
-        mpl_logger.setLevel(logging.WARNING)
-    import matplotlib.pylab as plt
-
-    fig, ax = plt.subplots(figsize=(6, 4))
-    im = ax.imshow(
-        alignment.transpose(),
-        aspect="auto",
-        origin="lower",
-        interpolation="none",
-    )
-    fig.colorbar(im, ax=ax)
-    xlabel = "Decoder timestep"
-    if info is not None:
-        xlabel += "\n\n" + info
-    plt.xlabel(xlabel)
-    plt.ylabel("Encoder timestep")
-    plt.tight_layout()
-
-    fig.canvas.draw()
-    data = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep="")
-    data = data.reshape((*fig.canvas.get_width_height()[::-1], 3))
-    plt.close()
-    return data
-
-
 def load_wav_to_torch(full_path):
     data, sampling_rate = librosa.load(full_path, sr=None)
     return torch.FloatTensor(data), sampling_rate
